b0e62709a3e3af4f5e7d317bb92d398acfb3582d
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2024-10-25 14:41:00 +0300

    Initial Commit.

---
+++ src/test/java/com/w/ever/files/manager/FilesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w.ever.files.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
