cab95022718bbf169002fc89a8b5c616afd64895
Bahaa AL-Deen AL-Nokta <b.alnokta@aldrtech.sy>
2024-11-07 18:20:31 +0300

    Add children Field.

--- src/main/java/com/w/ever/files/manager/models/FileModel.java
+++ src/main/java/com/w/ever/files/manager/models/FileModel.java
@@ -1,64 +1,41 @@
 package com.w.ever.files.manager.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "files")
+@Data
+@NoArgsConstructor
 public class FileModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(referencedColumnName = "id")
+    @JsonBackReference // Prevents recursive serialization of parent field
     private FileModel parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference // Indicates that this is the forward part of the relationship
+    private List<FileModel> children;
+
     private String name;
+
     private String path;
-    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+
+    @Column(name = "created_at")
     private LocalDateTime created_at;
 
-    public FileModel() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public FileModel getParent() {
-        return parent;
-    }
-
-    public void setParent(FileModel parent) {
-        this.parent = parent;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public LocalDateTime getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(LocalDateTime created_at) {
-        this.created_at = created_at;
+    @PrePersist
+    protected void onCreate() {
+        this.created_at = LocalDateTime.now();
     }
 }
